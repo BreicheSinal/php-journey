803fbb4c125a8789a4539c559af43ef7cfdc2d10
BreicheSinal <sinalbreiche@gmail.com>
2024-11-16 15:11:35 +0200

    Added a loop to get the reverse of the word

--- palindrome.php
+++ palindrome.php
@@ -2,12 +2,17 @@
 
 function isPalindrome($word){
     $wordLength = 0;
+    $reversedWord='';
 
     // finding the length of the word
     while(isset($word[$wordLength])){
         $wordLength++;
     }
 
+    // making the reverse of the word
+    for ($i = $wordLength - 1; $i >= 0; $i--){
+        $reversedWord .= $word[$i];
+    }
 
 }
 
