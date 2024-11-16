a1451492714a46a42f5a4239bea67c89784f2a63
BreicheSinal <sinalbreiche@gmail.com>
2024-11-16 22:05:57 +0200

    Checked array length before sorting

---
+++ merge_sort.php
@@ -0,0 +1,10 @@
+<?php
+
+function mergeSort($array) {
+    $arrLength = count( $array );
+
+    // array with one value is already sorted | base case
+    if ($arrLength < 2)
+        return $array;
+
+}
