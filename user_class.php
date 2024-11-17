a5d82aff1998b0db661e34e309797a3867b4faf0
BreicheSinal <sinalbreiche@gmail.com>
2024-11-17 22:12:37 +0200

    Added validation function of password

--- user_class.php
+++ user_class.php
@@ -1,13 +1,24 @@
 <?php
 
 class User{
+    // class attributes
     public $name;
     public $email;
     public $password;
 
+    // constructor
     public function __construct($name, $email, $password){
         $this->name = $name;
         $this->email = $email;
         $this->password = $password;
     }
+
+    // static function to validate pass(12char, 1upper/lower, 1specialChar)
+    public static function passwordValidation($password) {
+        $regex = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{12,}$/';
+        $result = preg_match($regex, $password);
+        return $result ? true : false;
+            
+    }
+
 }
