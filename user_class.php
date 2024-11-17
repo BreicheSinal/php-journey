0503186eaad4e51eb8fa753ab1fa5262648810e3
BreicheSinal <sinalbreiche@gmail.com>
2024-11-17 23:04:25 +0200

    Handled POST request

--- user_class.php
+++ user_class.php
@@ -1,5 +1,10 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+
 class User{
     // class attributes
     public $name;
@@ -59,3 +64,34 @@ class User{
     
     
 }
+
+    $requestData = json_decode(file_get_contents('php://input'), true);
+
+    $requestData = $_POST;
+
+    // getting data from request
+    $name = isset($requestData['name']) ? $requestData['name'] : null;
+    $email = isset($requestData['email']) ? $requestData['email'] : null;
+    $password = isset($requestData['password']) ? $requestData['password'] : null;
+
+    // validating email and password
+    $emailValidation = User::emailValidation($email);
+    $passwordValidation = User::passwordValidation($password);
+
+    if ($emailValidation === true && $passwordValidation === true) {
+        // creating new user instance(updated data)
+        $updatedUser = $currentUser->copy_with($name, $email, $password);
+
+        $response['success'] = true;
+        $response['message'] = 'USER SUCCESSFULLY UPDATED!';
+        $response['data'] = [
+            'name' => $updatedUser->name,
+            'email' => $updatedUser->email,
+            'password' => $updatedUser->password
+        ];
+    } else {
+        // returning validation error msg
+        $response['message'] = $emailValidation !== true ? $emailValidation : $passwordValidation;
+    }
+
+echo json_encode($response);
